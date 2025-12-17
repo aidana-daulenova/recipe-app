8208deab5f52ce99bb1e81f9bdbea6be07062667
Aidana Kassym <aidanadaulenova@gmail.com>
2025-12-17 10:48:57 +0200

    resolve conflict

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,13 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-=======
 import React from 'react'
 import ReactDOM from 'react-dom/client'
->>>>>>> main
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from '@/components/ui/provider'
 
-<<<<<<< HEAD
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-     <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-=======
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider>
       <App />
     </Provider>
   </React.StrictMode>,
->>>>>>> main
 )
