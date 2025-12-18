270b797524c0cf370e7bb31f0518fa922023592b
Aidana Kassym <aidanadaulenova@gmail.com>
2025-12-18 21:45:56 +0200

    Delete unnecessary comments

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,14 @@ import Signup from "./Pages/Signup";
 function App () {
   return (
     <>
-      {/* Навигация */}
+     
       <nav >
         <Link to="/">Home</Link>
         <Link to="/menu">Menu</Link>
         <Link to="/signup">Signup</Link>
       </nav>
 
-      {/* Роуты */}
+     
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
