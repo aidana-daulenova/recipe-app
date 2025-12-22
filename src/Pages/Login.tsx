c100c9cd59414bb34489eb09e29a1b7e4ddc2a35
Aidana Kassym <aidanadaulenova@gmail.com>
2025-12-22 22:45:26 +0200

    fix login page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -3,30 +3,30 @@ import { Button, Field, Fieldset, Input,  Stack, Center,} from "@chakra-ui/react
 function LoginPage() {
   return (
     <Center minH="100vh">
-    <Fieldset.Root size="lg" maxW="md">
-      <Stack>
-        <Fieldset.Legend>Log In page</Fieldset.Legend>
-        <Fieldset.HelperText>
-          Please enter your details
-        </Fieldset.HelperText>
-      </Stack>
+      <Fieldset.Root size="lg" maxW="md">
+        <Stack>
+          <Fieldset.Legend>Log in page</Fieldset.Legend>
+          <Fieldset.HelperText>
+            Please enter your details
+          </Fieldset.HelperText>
+        </Stack>
 
-      <Fieldset.Content>
-        <Field.Root>
-          <Field.Label>Your email</Field.Label>
-          <Input name="email" type="email" />
-        </Field.Root>
+        <Fieldset.Content>
+          <Field.Root>
+            <Field.Label>Your email</Field.Label>
+            <Input name="email" type="email" />
+          </Field.Root>
 
-        <Field.Root>
-          <Field.Label>Your password</Field.Label>
-          <Input name="password" type="password" />
-        </Field.Root>
-      </Fieldset.Content>
+          <Field.Root>
+            <Field.Label>Your password</Field.Label>
+            <Input name="password" type="password" />
+          </Field.Root>
+        </Fieldset.Content>
 
-      <Button type="submit" alignSelf="flex-start">
-        Log In
-      </Button>
-    </Fieldset.Root>
+        <Button type="submit" alignSelf="flex-start">
+          Log In
+        </Button>
+      </Fieldset.Root>
     </Center>
   )
 };
