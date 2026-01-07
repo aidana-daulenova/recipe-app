13eb0a221639f50087a1a0a05c7a96c84f946d90
Aidana Kassym <aidanadaulenova@gmail.com>
2026-01-07 14:45:01 +0200

    create logo and update login

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,10 +1,33 @@
-import { Link } from "react-router-dom"
+import { Flex, Image, Link as ChakraLink } from "@chakra-ui/react"
+import { Link as RouterLink} from "react-router-dom"
+import logo from "../assets/logo.png"
 
 function TopBar() {
   return (
-    <div>
-      <Link to="/login">Login</Link>
-    </div>
+    <Flex
+    align="center"
+    justify="space-between"
+    px={10}
+    >
+
+      <ChakraLink as={RouterLink} to="/" >
+        <Image
+          src={logo}
+          alt="MyRecipes logo"
+          height="75px"
+          objectFit="contain"
+        />
+      </ChakraLink>
+
+      <ChakraLink
+        as={RouterLink}
+        to="/login"
+        fontWeight="medium"
+        _hover={{ textDecoration: "underline" }}
+      >
+        Log In
+      </ChakraLink>
+    </Flex>
   )
 }
 
