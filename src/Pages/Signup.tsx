723dc5adf68c8f07fb3bc3f66f95d7b81e76eb0d
Aidana Kassym <aidanadaulenova@gmail.com>
2026-01-20 15:10:21 +0200

    fix extra space

--- src/Pages/Signup.tsx
+++ src/Pages/Signup.tsx
@@ -3,27 +3,29 @@ import {Button, Field, Fieldset, Input, Stack, Center, Text} from "@chakra-ui/re
 export default function Signup() {
   return (
     <Center minH="100vh">
-        <Fieldset.Root size="lg" maxW="md">
-      <Stack>
-        <Fieldset.Legend><Text textStyle="4xl">Create an account</Text></Fieldset.Legend>
-      </Stack>
+      <Fieldset.Root size="lg" maxW="md">
+        <Stack>
+          <Fieldset.Legend>
+            <Text textStyle="4xl">Create an account</Text>
+          </Fieldset.Legend>
+        </Stack>
 
-      <Fieldset.Content>
-        <Field.Root>
-          <Field.Label>Name</Field.Label>
-          <Input name="name" />
-        </Field.Root>
+        <Fieldset.Content>
+          <Field.Root>
+            <Field.Label>Name</Field.Label>
+            <Input name="name" />
+          </Field.Root>
 
-        <Field.Root>
-          <Field.Label>Email address</Field.Label>
-          <Input name="email" type="email" />
-        </Field.Root>
-      </Fieldset.Content>
+          <Field.Root>
+            <Field.Label>Email address</Field.Label>
+            <Input name="email" type="email" />
+          </Field.Root>
+        </Fieldset.Content>
 
-      <Button type="submit" alignSelf="flex-start">
-        Join now
-      </Button>
-    </Fieldset.Root>
+        <Button type="submit" alignSelf="flex-start">
+          Join now
+        </Button>
+      </Fieldset.Root>
     </Center> 
   )
 }
