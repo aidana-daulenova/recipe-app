0c2c83d3dabd430b89a5720b90b74e83beadba15
Aidana Kassym <aidanadaulenova@gmail.com>
2026-02-02 15:06:48 +0200

    style: improve main menu design

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,11 +1,46 @@
 import {Stack, Link as ChakraLink} from "@chakra-ui/react"
+import { useLocation } from "react-router-dom"
 
 export default function HealthyMenu() {
+  const location = useLocation();
+
   return (
     <Stack align="flex-start" spacing={3}>
-      <ChakraLink href="/recipes" px={2} py={1} borderRadius="md" _hover={{ bg: "gray.200" }}>Recipes</ChakraLink>
-      <ChakraLink href="/forum" px={2} py={1} borderRadius="md" _hover={{ bg: "gray.200" }}>Forum</ChakraLink>
-      <ChakraLink href="/about-us" px={2} py={1} borderRadius="md" _hover={{ bg: "gray.200" }}>About us</ChakraLink>
+      <ChakraLink 
+      href="/recipes" 
+      minW="150px"
+      px={2} 
+      py={1} 
+      borderRadius="md" 
+      bg={location.pathname === "/recipes" ? "teal.100" : "transparent"}
+      _hover={{ bg: 'gray.200', textDecoration: "none" }}
+      >
+        Recipes
+      </ChakraLink>
+
+      <ChakraLink 
+      minW="150px"
+      href="/forum" 
+      px={2} 
+      py={1} 
+      borderRadius="md" 
+      bg={location.pathname === "/forum" ? "teal.100" : "transparent"}
+      _hover={{ bg: 'gray.200', textDecoration: "none" }}
+      >
+        Forum
+      </ChakraLink>
+
+      <ChakraLink 
+      href="/about-us" 
+      minW="150px"
+      px={2} 
+      py={1} 
+      borderRadius="md" 
+      bg={location.pathname === "/about-us" ? "teal.100" : "transparent"}
+      _hover={{ bg: 'gray.200', textDecoration: "none" }}
+      >
+        About Us
+      </ChakraLink>
     </Stack>
   )
 }
