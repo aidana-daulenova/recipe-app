a4c3de6d3b3948e5f7fedde23244593203751e6a
Aidana Kassym <aidanadaulenova@gmail.com>
2026-03-24 23:02:57 +0200

    style: delete empty space between topbar and main menu  & change hover animation

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,31 +1,26 @@
 import { Outlet } from "react-router-dom";
 import TopBar from "@/components/TopBar";
 import MainMenu from "@/components/MainMenu";
-import { Box, VStack, Flex } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import Footer from "@/components/Footer";
 
 export default function MainLayout() {
   return (
     <Flex direction="column" h="100vh" w="full" overflow="hidden">
-      {/* TopBar */}
-      <Box w="full" h="100px" flexShrink={0}>
+      <Box w="full" flexShrink={0}>
         <TopBar />
       </Box>
 
-      {/* Main content */}
-      <Flex flex={1} w="full" gap={4} px={8} overflow="hidden">
-        {/* Sidebar */}
+      <Flex flex={1} w="full" gap={4} px={8} pt={1} overflow="hidden">
         <Box flex={1} overflowY="auto">
           <MainMenu />
         </Box>
 
-        {/* Main content */}
         <Box flex={6} px={8} overflowY="auto">
           <Outlet />
         </Box>
       </Flex>
 
-      {/* Footer */}
       <Box flexShrink={0}>
         <Footer />
       </Box>
