d795a596f48fda7823aad1451d40f1307cbbd61d
Aidana Kassym <aidanadaulenova@gmail.com>
2026-05-13 22:49:44 +0300

    move ingredients to mock file

--- src/mock.ts
+++ src/mock.ts
@@ -64,3 +64,11 @@ export const quickRecipes = [
     time: "20 mins",
   },
 ];
+
+export const Ingredients = [
+  { id: 1, label: "Milk", units: "ml" },
+  { id: 2, label: "Flour", units: "g" },
+  { id: 3, label: "Eggs", units: "pcs" },
+  { id: 4, label: "Butter", units: "g" },
+  { id: 5, label: "Banana", units: "pcs" },
+];
