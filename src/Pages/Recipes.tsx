df019e63c7162e18ff49825c1e1ce5f74730f214
Aidana Kassym <aidanadaulenova@gmail.com>
2026-06-16 12:35:13 +0300

    feat: add search functionality to pages

--- src/Pages/Recipes.tsx
+++ src/Pages/Recipes.tsx
@@ -9,12 +9,19 @@ import {
 } from "@chakra-ui/react";
 import { useRecipe } from "@/hooks/useRecipe";
 import { LuSearch } from "react-icons/lu";
+import { useState } from "react";
+import { MdOutlineSearchOff } from "react-icons/md";
 
 export default function Recipes() {
   const recipes = useRecipe((state) => state.recipes);
+  const [query, setQuery] = useState("");
+
+  const filtered = recipes.filter((recipe) =>
+    recipe.title.toLowerCase().includes(query.toLowerCase()),
+  );
 
   return (
-    <Box p="3">
+    <Box p="1">
       <Flex mt="2" direction="row-reverse">
         <InputGroup
           startElement={<LuSearch />}
@@ -22,44 +29,56 @@ export default function Recipes() {
           shadow="md"
           borderRadius="md"
         >
-          <Input placeholder="Search..." size="md" />
+          <Input
+            placeholder="Search..."
+            size="md"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
         </InputGroup>
       </Flex>
 
-      <Grid templateColumns="repeat(4, 1fr)" gap="9" pt={3}>
-        {recipes.map((recipe) => (
-          <Flex
-            key={recipe.id}
-            direction={"column"}
-            borderRadius="xl"
-            gap="2"
-            transition="all 0.3s ease"
-            _hover={{
-              transform: "scale(0.97)",
-              bg: "white",
-            }}
-            cursor="pointer"
-          >
-            <Image
-              src={recipe.imageUrl}
-              h="300px"
-              w="100%"
-              objectFit="cover"
-              objectPosition="center"
-              borderRadius="4xl"
-            />
-
-            <Text
-              fontSize="xl"
-              textTransform="uppercase"
-              as="b"
-              color="teal.700"
+      {filtered.length === 0 ? (
+        <Flex justify="center" align="center" minH="60vh" gap={2}>
+          <MdOutlineSearchOff size={60} />
+          <Text textStyle="4xl">Not found</Text>{" "}
+        </Flex>
+      ) : (
+        <Grid templateColumns="repeat(4, 1fr)" gap="9" pt={3}>
+          {filtered.map((recipe) => (
+            <Flex
+              key={recipe.id}
+              direction={"column"}
+              borderRadius="xl"
+              gap="2"
+              transition="all 0.3s ease"
+              _hover={{
+                transform: "scale(0.97)",
+                bg: "white",
+              }}
+              cursor="pointer"
             >
-              {recipe.title}
-            </Text>
-          </Flex>
-        ))}
-      </Grid>
+              <Image
+                src={recipe.imageUrl}
+                h="300px"
+                w="100%"
+                objectFit="cover"
+                objectPosition="center"
+                borderRadius="4xl"
+              />
+
+              <Text
+                fontSize="xl"
+                textTransform="uppercase"
+                as="b"
+                color="teal.700"
+              >
+                {recipe.title}
+              </Text>
+            </Flex>
+          ))}
+        </Grid>
+      )}
     </Box>
   );
 }
