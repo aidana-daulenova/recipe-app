6e8b19c53901e7fa966f201687d8b669907f00be
Aidana Kassym <aidanadaulenova@gmail.com>
2026-06-16 11:31:04 +0300

    add search bar to recipes page

--- src/Pages/Recipes.tsx
+++ src/Pages/Recipes.tsx
@@ -1,38 +1,65 @@
-import { Flex, Grid, Image, Text } from "@chakra-ui/react";
+import {
+  Flex,
+  Grid,
+  Image,
+  Text,
+  Box,
+  InputGroup,
+  Input,
+} from "@chakra-ui/react";
 import { useRecipe } from "@/hooks/useRecipe";
+import { LuSearch } from "react-icons/lu";
 
 export default function Recipes() {
   const recipes = useRecipe((state) => state.recipes);
 
   return (
-    <Grid templateColumns="repeat(4, 1fr)" gap="9" pt={3}>
-      {recipes.map((recipe) => (
-        <Flex
-          key={recipe.id}
-          direction={"column"}
-          borderRadius="xl"
-          gap="2"
-          transition="all 0.3s ease"
-          _hover={{
-            transform: "scale(0.97)",
-            bg: "white",
-          }}
-          cursor="pointer"
+    <Box p="3">
+      <Flex mt="2" direction="row-reverse">
+        <InputGroup
+          startElement={<LuSearch />}
+          maxW="25%"
+          shadow="md"
+          borderRadius="md"
         >
-          <Image
-            src={recipe.imageUrl}
-            h="300px"
-            w="100%"
-            objectFit="cover"
-            objectPosition="center"
-            borderRadius="4xl"
-          />
+          <Input placeholder="Search..." size="md" />
+        </InputGroup>
+      </Flex>
 
-          <Text fontSize="xl" textTransform="uppercase" as="b" color="teal.700">
-            {recipe.title}
-          </Text>
-        </Flex>
-      ))}
-    </Grid>
+      <Grid templateColumns="repeat(4, 1fr)" gap="9" pt={3}>
+        {recipes.map((recipe) => (
+          <Flex
+            key={recipe.id}
+            direction={"column"}
+            borderRadius="xl"
+            gap="2"
+            transition="all 0.3s ease"
+            _hover={{
+              transform: "scale(0.97)",
+              bg: "white",
+            }}
+            cursor="pointer"
+          >
+            <Image
+              src={recipe.imageUrl}
+              h="300px"
+              w="100%"
+              objectFit="cover"
+              objectPosition="center"
+              borderRadius="4xl"
+            />
+
+            <Text
+              fontSize="xl"
+              textTransform="uppercase"
+              as="b"
+              color="teal.700"
+            >
+              {recipe.title}
+            </Text>
+          </Flex>
+        ))}
+      </Grid>
+    </Box>
   );
 }
